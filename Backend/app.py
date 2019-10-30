6f45074e6c7b8d787ef88c8c55f7b1ecd2469387
Austin Yuan <austinyuan2@gmail.com>
2019-10-30 16:07:00 -0700

    fixed corbs

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, json
+from flask import Flask, json, make_response
 app = Flask(__name__)
 
 # Retrieve tutors given class ID
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def get_tutors(classid):
     data = {}
     if classid == 'CSE128':
-        data = {"tutors":[{"name":"Austin Yuan","grade":"A"}, {"name":"Priya Rajarathinam","grade":"C"}]}
+        data = {"tutors":[{"name":"Austin Yuan","grade":"A"}, {"name":"Priya Rajarathinam","grade":"A"}]}
     elif classid == 'CSE140':
         data = {"tutors":[{"name":"Fizz Buzz","grade":"B+"}]}
-    return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')   
+    return app.response_class(response=json.dumps(data),status=200,mimetype='application/json', headers={'Access-Control-Allow-Origin': '*'})   
--- Website/js/fetchData.js
+++ Website/js/fetchData.js
@@ -1,29 +1,44 @@
 // This script is used to replace HTML data with dynamic data
 
 //Map values to html elements
-var valToHTML = {}  //Syntax: ["elementID", "displayName"]
-valToHTML["Energy"] = ["#energy", "Calories"];
-valToHTML["Sugars, total"] = ["#sugars", "Sugars"];
-valToHTML["Total lipid (fat)"] = ["#lipids", "Fats"];
-valToHTML["Carbohydrate, by difference"] = ["#carbs", "Carbohydrates"];
+let valToHTML = {}  //Syntax: [`elementID`, `displayName`]
+valToHTML[`Energy`] = [`#energy`, `Calories`];
+valToHTML[`Sugars, total`] = [`#sugars`, `Sugars`];
+valToHTML[`Total lipid (fat)`] = [`#lipids`, `Fats`];
+valToHTML[`Carbohydrate, by difference`] = [`#carbs`, `Carbohydrates`];
 
 //Config
-var databasekey = "10SKlZqWe3IkC3ymxUWxzMrjgUfNFuixcuqY10gC";
-var dbnum = `01009`;
-var ntCategories = '&nutrients=205&nutrients=204&nutrients=208&nutrients=269';
+let databasekey = `10SKlZqWe3IkC3ymxUWxzMrjgUfNFuixcuqY10gC`;
+let dbnum = `01009`;
+let ntCategories = `&nutrients=205&nutrients=204&nutrients=208&nutrients=269`;
+let flaskURL = `localhost:5000`;
 
-function searchItem(searchTerm) {
-  var searchQuery = `http://api.nal.usda.gov/ndb/search/?format=json&q=${searchTerm}&sort=r&max=5&offset=0&api_key=${databasekey}`;
+function searchTutors(searchTerm) {
+  let searchQuery = `http://${flaskURL}/tutors/${searchTerm}`;
+
+  $.get(searchQuery, function(data) {
+    if (data[`tutors`] === undefined) {
+      $(name).html(`<b>No tutor found for class: ${searchTerm}<\b>`)
+      return;
+    }
+    let tutors = data[`tutors`]
+    console.log(tutors);
+    $("name").html(tutors[0][`name`]);
+  })
+}
+
+function searchItemExample(searchTerm) {
+  let searchQuery = `http://api.nal.usda.gov/ndb/search/?format=json&q=${searchTerm}&sort=r&max=5&offset=0&api_key=${databasekey}`;
   $.get(searchQuery, function(data){
     let selectIndex = 0;
     let selectedItem = data.list.item[selectIndex];
-    console.log("Database entry: " + selectedItem.name);
-    var nutritionQuery = `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${databasekey}${ntCategories}&ndbno=${selectedItem.ndbno}`;
+    console.log(`Database entry: ` + selectedItem.name);
+    let nutritionQuery = `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=${databasekey}${ntCategories}&ndbno=${selectedItem.ndbno}`;
     $.get(nutritionQuery, function(data) {
-      $(foodName).html("<b>Nutritional Data for " + formatTitle(selectedItem.name) + "<b>");
+      $(foodName).html(`<b>Nutritional Data for ` + formatTitle(selectedItem.name) + `<b>`);
       data.report.foods[0].nutrients.forEach((e) => {
-        console.log(valToHTML[e.nutrient][0] + ": " + e.value);
-        $(valToHTML[e.nutrient][0]).html("<b>" + valToHTML[e.nutrient][1] + ": </b>" + e.value + " grams");
+        console.log(valToHTML[e.nutrient][0] + `: ` + e.value);
+        $(valToHTML[e.nutrient][0]).html(`<b>` + valToHTML[e.nutrient][1] + `: </b>` + e.value + ` grams`);
       })
     })
   })
@@ -31,7 +46,7 @@ function searchItem(searchTerm) {
 
 function formatTitle(text) {  //Removes UPC and formats capitalization
   return text.toLowerCase()
-    .split(' ')
+    .split(` `)
     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-    .join(' ');
+    .join(` `);
 }
