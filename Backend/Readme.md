56dfc3224b1a1ef73205165b8ae7ca70e7b90562
Austin Yuan <austinyuan2@gmail.com>
2019-11-08 16:30:22 -0800

    Pisa search api server added

---
+++ Backend/Readme.md
@@ -0,0 +1,8 @@
+# Backend
+The backend consists of two API servers.
+
+## PisaSearchAPI
+An API server that interacts with UCSC's Pisa course website. It is used for retrieving class information. 
+
+## WebEndpoints
+The API server that website users directly interact with. It will handle retrieving user data, tutoring information, and other information from our database.
--- Backend/WebEndpoints/app.py
+++ Backend/WebEndpoints/app.py
@@ -3,14 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import MetaData
 from flask import Flask, json, make_response
+from flask_cors import CORS
+
+WEB_ENDPOINT_PORT = 5000
 
 app = Flask(__name__)
+CORS(app)
 meta = MetaData()
 engine = create_engine('sqlite:///sqlalchemy_example.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 db = DBSession()
-print("created session")
+
 # Retrieve tutors given class ID
 @app.route('/tutors/<classid>', methods=['GET'])
 def get_tutors(classid):
@@ -21,4 +25,7 @@ def get_tutors(classid):
     for tutor in tutors:
         tutor_dict.append({"name":tutor.name})
     data["tutors"] = tutor_dict 
-    return app.response_class(response=json.dumps(data),status=200,mimetype='application/json', headers={'Access-Control-Allow-Origin': '*'})  
+    return app.response_class(response=json.dumps(data),status=200,mimetype='application/json')  
+
+if __name__ == '__main__':
+    app.run(port=WEB_ENDPOINT_PORT)
