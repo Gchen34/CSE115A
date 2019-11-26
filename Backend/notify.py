d91ff99c8f67e0983dd51b9fece4ae5d2f4d6892
Priya Rajarathinam <cat@eduroam-169-233-205-198.ucsc.edu>
2019-11-26 09:53:23 -0800

    email format

---
+++ Backend/notify.py
@@ -0,0 +1,36 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sender_email = "slugstudy2019@gmail.com"
+receiver_email = "ajyuan@ucsc.edu"
+password = input("Type your password and press enter:")
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "Tutor Request"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+# Create the plain-text and HTML version of your message
+text = """\
+Hello {},
+{} has reached out to you.
+Here is their email if you'd like to contact them. 
+{}
+"""
+
+text = text.format("Name1","Name2","email1") 
+# Turn these into plain/html MIMEText objects
+part1 = MIMEText(text, "plain")
+
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part1)
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
