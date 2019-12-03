67e1281d3d2b1dc589b0ca2ecc4a9ca0aa6c998f
PriyaRajarathinam <35835697+PriyaRajarathinam@users.noreply.github.com>
2019-12-02 23:58:47 -0800

    removed unecessary files (#40)

--- Website/js/userhandler.js
+++ Website/js/userhandler.js
@@ -1,16 +1,5 @@
 // Get user data
 
-var db = openDatabase('mydb', '1.0', 'User database', 2 * 1024 * 1024);
-
-db.transaction(function (tx) {
-    //tx.executeSql('DROP TABLE users');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS users (email unique, password, name, calories DEFAULT 0, sugar DEFAULT 0, fats DEFAULT 0, carbohydrates DEFAULT 0)');
-    //tx.executeSql('CREATE TABLE IF NOT EXISTS nt (email unique, calories, sugar, fats, carbohydrates)')
-    tx.executeSql('SELECT * FROM users', [], function (tx, results) {
-        var len = results.rows.length, i;
-    });
-    console.log("user handler opened!")
-});
   
 function addUser(name, email, password) {
     firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error){
@@ -44,23 +33,6 @@ function signOutUser() {
     console.error('Sign Out Error', error);
     });
     
-}
-function updateUser(calories = 0, sugar = 0, fats = 0, carbs = 0) {
-    console.log("Values: " + calories + " " + sugar + " " + fats + " " + carbs + " " + localStorage.getItem('user'))
-    db.transaction(function (tx) {
-        tx.executeSql('UPDATE users SET calories=calories+?,sugar=sugar+?,fats=fats+?,carbohydrates=carbohydrates+? WHERE email=?', [calories, sugar, fats, carbs, localStorage.getItem('user')],
-            function (tx, results) { console.log("Success") }, function (transaction, error) { console.log(error); }
-        );
-    })
-}
-
-function updateUserData() {
-    db.transaction(function (tx) {
-        tx.executeSql('SELECT name,calories,sugar,fats,carbohydrates FROM users WHERE email=?', [localStorage.getItem('user')], function (tx, results) {
-            let ud = results.rows.item(0);
-            $("#userStats").html(`Good afternoon ${ud.name}. Today you ate <b>${ud.carbohydrates.toFixed(1)} grams of carbohydrates, ${ud.sugar.toFixed(1)} grams of sugar, </b>and<b> ${ud.fats.toFixed(1)} grams of fat.</b>You are at <b>${(ud.calories / 2400 * 100).toFixed(1)}% of your daily calorie goal</b>. Have a great day :>`);
-        })
-    });
 }
 
 function setGreeting() {
